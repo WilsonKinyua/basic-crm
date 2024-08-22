75357d24c7bf154af343e9b75fb125a3252e363e
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-08-23 02:41:43 +0300

    feat: Add loading state and error handling in CustomerForm. Enhances UX by indicating form submission progress and showing error messages based on response.

--- src/components/CustomerForm.vue
+++ src/components/CustomerForm.vue
@@ -2,11 +2,13 @@
 import { toast } from 'vue-sonner'
 import { useStore } from 'vuex';
 import { useForm } from 'vee-validate'
+import { Loader2 } from "lucide-vue-next"
 import { Input } from '@/components/ui/input';
 import { toTypedSchema } from '@vee-validate/zod'
 import * as z from 'zod'
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Button } from '@/components/ui/button'
+import { ref } from 'vue';
 
 // form schema
 const schema = z.object({
@@ -27,14 +29,34 @@ const { handleSubmit, values, resetForm } = useForm({
 })
 
 const store = useStore();
+const isLoading = ref(false);
+const emit = defineEmits(['formSubmitted']); // emit event to parent component
 
-const onSubmit = handleSubmit((values) => {
-    // create customer
-    store.dispatch('customers/createCustomer', values)
-    // show success message
-    toast.success('Customer created successfully')
-    // reset form
-    resetForm()
+const onSubmit = handleSubmit(async (values) => {
+    isLoading.value = true;
+    try {
+        // create customer
+        await store.dispatch('customers/createCustomer', values)
+        // show success message
+        toast.success('Customer created successfully')
+        // reset form
+        resetForm()
+        // emit event to parent component
+        emit('formSubmitted')
+    } catch (error: any) {
+        console.log(error);
+
+        // check if error response exists and has a message
+        if (error.response && error.response.data && error.response.data.message) {
+            // show error message from response
+            toast.error(error.response.data.message.join(', '));
+        } else {
+            // show generic error message
+            toast.error('Failed to create customer. Please try again');
+        }
+    } finally {
+        isLoading.value = false;
+    }
 })
 
 </script>
@@ -76,8 +98,12 @@ const onSubmit = handleSubmit((values) => {
                 <FormMessage />
             </FormItem>
         </FormField>
-        <Button type="submit" class="w-full">
-            Create Customer
+        <Button type="submit" :disabled="isLoading" class="w-full">
+            <div v-if="isLoading" class="flex items-center justify-center">
+                Please wait
+                <Loader2 class="w-4 h-4 mr-2 animate-spin ml-2" />
+            </div>
+            <span v-else>Create Customer</span>
         </Button>
     </form>
 </template>
