6cb34e22f3b752e051aacc7d2b0ffb37601224c7
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-08-23 00:01:13 +0300

    feat: Add Vuex store modules for customers and leads; handle CRUD operations

---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+import { createStore } from 'vuex'
+import { customers } from './modules/customers';
+import { leads } from './modules/leads';
+
+export default createStore({
+    modules: {
+        customers,
+        leads
+    },
+});
